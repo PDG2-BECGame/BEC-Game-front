5a408d43f7860a72e5faabea3540efffa752cc74
Jorge Eduardo Jojoa <jorgeeduardo2410@gmail.com>
2024-11-01 21:54:45 -0500

    Fixed errors

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AuthLayout from './features/Auth/pages/AuthLayout';
-import Login from './features/Auth/pages/Login';
+import Login from './features/Auth/pages/auth/Login';
 import reportWebVitals from './reportWebVitals';
 import LoginView from './features/Auth/pages/auth/LoginView';
 
