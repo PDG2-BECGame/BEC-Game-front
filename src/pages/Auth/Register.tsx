3b2f7600bc471f3d8d83cbdbcb876c4a45336dd2
Felipe Castillo <1006071586@u.icesi.edu.co>
2024-11-26 22:08:40 -0500

    enhance: apply design to register - font adjustments pending for consistency with login

--- src/pages/Auth/Register.tsx
+++ src/pages/Auth/Register.tsx
@@ -23,128 +23,147 @@ const Register: React.FC = () => {
   } = useRegister();
 
   return (
-    <div className="min-h-screen flex flex-col md:flex-row">
+    <div className="min-h-screen flex">
       {/* Columna Izquierda */}
-      <div className="md:w-1/2 w-full bg-blue-600 text-white flex flex-col justify-center items-center p-8">
-        <img src={BePROLogo} alt="BePRO Logo" className="h-20 mb-6" />
-        <h1 className="text-5xl font-bold mb-4">Bienvenido a BePRO</h1>
-        <p className="text-lg text-center">
-          Únete a nuestra comunidad y comienza tu camino hacia el éxito profesional.
-        </p>
+      <div className="hidden lg:flex lg:w-1/2 bg-gradient-to-r from-customBlue to-customPurple text-white flex-col items-start justify-center p-8">
+        <div className="px-8">
+          {/* Logo y Título */}
+          <div className="flex items-center mb-6">
+            <img src={BePROLogo} alt="BePRO Logo" className="h-12 w-12 mr-4" />
+            <h1 className="text-4xl font-bold">BEPRO</h1>
+          </div>
+
+          {/* Frase Principal */}
+          <h2 className="text-5xl font-bold mb-4">Domina, detecta, defiende</h2>
+
+          {/* Descripción */}
+          <p className="text-lg">
+            Juego serio para capacitar a empleados en la detección de técnicas de ingeniería social y reducir ataques de compromiso de correo electrónico empresarial (BEC).
+          </p>
+        </div>
       </div>
 
       {/* Columna Derecha */}
-      <div className="md:w-1/2 w-full flex flex-col justify-center items-center p-8">
-        <h2 className="text-3xl font-bold mb-6">Crear una cuenta</h2>
-        <form onSubmit={handleRegister} className="w-full max-w-md">
-          {error && <p className="text-red-500 mb-4">{error}</p>}
-
-          <div className="mb-4">
-            <label htmlFor="name" className="block text-gray-700">
-              Nombre Completo
-            </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              placeholder="Ingresa tu nombre"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-              required
-            />
+      <div className="w-full lg:w-1/2 flex flex-col justify-center items-center p-8">
+        <div className="max-w-md w-full">
+          {/* Logo y Título en pantallas pequeñas */}
+          <div className="lg:hidden flex items-center justify-center mb-6">
+            <img src={BePROLogo} alt="BePRO Logo" className="h-12 w-12 mr-4" />
+            <h1 className="text-4xl font-bold">BEPRO</h1>
           </div>
 
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700">
-              Correo Electrónico
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Ingresa tu correo electrónico"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-              required
-            />
-          </div>
+          <h2 className="text-3xl font-bold mb-6">Crear una cuenta</h2>
+          {error && (
+            <div className="bg-red-100 text-red-700 p-2 rounded mb-4">
+              <p>{error}</p>
+            </div>
+          )}
+          <form onSubmit={handleRegister} className="w-full">
+            {/* Nombre Completo */}
+            <div className="mb-4">
+              <label htmlFor="name" className="block text-gray-700">
+                Nombre Completo
+              </label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Ingresa tu nombre"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-customBlue"
+                required
+              />
+            </div>
 
-          <div className="mb-4">
-            <label htmlFor="password" className="block text-gray-700">
-              Contraseña
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="Ingresa tu contraseña"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-              required
-            />
-          </div>
+            {/* Correo Electrónico */}
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-gray-700">
+                Correo Electrónico
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Ingresa tu correo electrónico"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-customBlue"
+                required
+              />
+            </div>
 
-          <div className="mb-4">
-            <label htmlFor="confirmPassword" className="block text-gray-700">
-              Confirmar Contraseña
-            </label>
-            <input
-              type="password"
-              name="confirmPassword"
-              id="confirmPassword"
-              placeholder="Confirma tu contraseña"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-              required
-            />
-          </div>
+            {/* Contraseña */}
+            <div className="mb-4">
+              <label htmlFor="password" className="block text-gray-700">
+                Contraseña
+              </label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Ingresa tu contraseña"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-customBlue"
+                required
+              />
+            </div>
 
-          {/* Organización */}
-          <div className="mb-4">
-            <label className="block text-gray-700 mb-2">
-              Organización (Opcional)
-            </label>
-            <select
-              value={organization}
-              onChange={(e) => setOrganization(e.target.value)}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500 mb-2"
-              disabled={!!newOrganization}
+            {/* Confirmar Contraseña */}
+            <div className="mb-4">
+              <label htmlFor="confirmPassword" className="block text-gray-700">
+                Confirmar Contraseña
+              </label>
+              <input
+                type="password"
+                id="confirmPassword"
+                placeholder="Confirma tu contraseña"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-customBlue"
+                required
+              />
+            </div>
+
+            {/* Organización */}
+            <div className="mb-4">
+              <label className="block text-gray-700 mb-2">Organización (Opcional)</label>
+              <select
+                value={organization}
+                onChange={(e) => setOrganization(e.target.value)}
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-customBlue mb-2"
+                disabled={!!newOrganization}
+              >
+                <option value="">Seleccione una organización</option>
+                {organizations.map((org) => (
+                  <option key={org.id} value={org.id}>
+                    {org.name}
+                  </option>
+                ))}
+              </select>
+              <input
+                type="text"
+                placeholder="Crear nueva organización"
+                value={newOrganization}
+                onChange={(e) => setNewOrganization(e.target.value)}
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-customBlue"
+                disabled={!!organization}
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-customBlue text-white py-2 rounded-lg hover:bg-blue-700 transition duration-300"
             >
-              <option value="">Seleccione una organización</option>
-              {organizations.map((org) => (
-                <option key={org.id} value={org.id}>
-                  {org.name}
-                </option>
-              ))}
-            </select>
-            <input
-              type="text"
-              placeholder="Crear nueva organización"
-              value={newOrganization}
-              onChange={(e) => setNewOrganization(e.target.value)}
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
-              disabled={!!organization}
-            />
-          </div>
+              Registrarse
+            </button>
+          </form>
 
-          <button
-            type="submit"
-            className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition duration-300"
-          >
-            Registrarse
-          </button>
-        </form>
-
-        <p className="mt-4 text-gray-600">
-          ¿Ya tienes una cuenta?{' '}
-          <Link to="/login" className="text-blue-600 hover:underline">
-            Inicia sesión aquí
-          </Link>
-        </p>
+          <p className="mt-4 text-gray-600">
+            ¿Ya tienes una cuenta?{' '}
+            <Link to="/login" className="text-customBlue hover:underline">
+              Inicia sesión aquí
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
